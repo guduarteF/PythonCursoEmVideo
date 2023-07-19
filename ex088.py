a43bec853b39a80ea44486ec429862c2c1e1f9b0
Gustavo Duarte <gustavo64duarte@gmail.com>
2023-07-19 16:27:46 -0300

    aula 017 ,[Resolução] ex088-ex089 , MUNDO 3

--- ex088.py
+++ ex088.py
@@ -5,18 +5,36 @@ O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números e
 from random import randint
 from time import sleep
 
-# NÃO TERMINADO
 print('-' * 30)
 print('{:^30}'.format('JOGA NA MEGA SENA'))
 print('-' * 30)
 jogos = list()
+verificacao = True
+qtd = int(input('Quantos jogos você quer que eu sorteie? '))
+print(f' -=-=-= SORTEANDO {qtd} JOGOS -==-=-')
+for i in range(1, qtd+1):
+    verificacao = True
+    print(f'Jogo {i}: ', end='')
+    for num in range(0, 6):
+        jogos.append(randint(0, 60))
 
+    # Para verificar se todos os numeros são diferentes entre sí
+    # Um for dentro do outro pra todos os números serem comparados
+    # De uma posição x para todas as posições y
+    # While para garantir que o número alterado não seja igual a outro numero na lista
 
-for num in range(0, 6):
-    jogo1.append(randint(1, 60))
-    jogo2.append(randint(1, 60))
-    jogo3.append(randint(1, 60))
-    jogo4.append(randint(1, 60))
+    while verificacao:
+        verificacao = False
+        for x in range(0, 6):
+            for y in range(0, 6):
+                if x != y:
+                    if jogos[x] == jogos[y]:
+                        jogos.pop(x)
+                        jogos.insert(x, randint(0, 60))
+                        verificacao = True
+                        break
 
-
-
+    print(jogos)
+    jogos.clear()
+    sleep(.5)
+print('-=' * 5 + ' <  BOA SORTE!  > ' + '-=' * 5)
---
+++ ex089.py
@@ -0,0 +1,41 @@
+""" Crie um programa que leia o nome e duas notas de vários alunos
+e guarde tudo em uma lista composta . No final , mostre um boletim
+contendo a média de cada um e permita que o usuário possa mostrar as
+notas de cada aluno individualmente. """
+
+# ALUNOS  [ [ [FULANO], [5.5, 8.0], [media]],  [[BELTRANO], [7.9, 4.5], [media]] ]
+alunos = list()
+dados = list()
+notas = list()
+medias = list()
+nomes = list()
+num = 0
+while True:
+    nomes.append(input('Nome: '))
+    notas.insert(0, float(input('Nota 1: ')))
+    notas.insert(1, float(input('Nota 2: ')))
+    medias.append(((notas[0] + notas[1]) / 2))
+    dados.append((nomes[:], notas[:], medias[:]))
+    alunos.append(dados[:])
+    nomes.clear()
+    notas.clear()
+    medias.clear()
+    dados.clear()
+    continuar = str(input('Quer continuar? [S/N]')).strip().upper()
+    if continuar != 'S':
+        break
+
+print('-=' * 15)
+print('No.  NOME        MÉDIA')
+print('-' * 15)
+for num in range(0, len(alunos)):
+    print(f'{num}  {alunos[num][0][0]}  {alunos[num][0][2]}  ')
+print('-' * 15)
+while True:
+    num = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if num == 999:
+        break
+    print(f'Notas de {alunos[num][0][0]} são {alunos[num][0][1]}')
+print('FINALIZANDO...')
+print('<<< VOLTE SEMPRE >>>')
+
