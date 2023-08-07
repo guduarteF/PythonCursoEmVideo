eb80d636ddfad1346fd9c9ce487ad3c3c78424cc
Gustavo Duarte <gustavo64duarte@gmail.com>
2023-08-07 19:24:40 -0300

    aula 23 ,[Resolução] ex115 , MUNDO 3

---
+++ ex115/sistema.py
@@ -0,0 +1,53 @@
+""" Crie um pequeno modularizado que permita cadastrar pessoas pelo seu nome e idade
+em um arquivo de texto simples.
+O sistema só vai ter 2 opções: cadastrar uma nova pessoa e listar todas as pessoas cadastradas."""
+
+from time import sleep
+
+
+def exibir(txt):
+    print('-' * 45)
+    print(f'{txt:^45}')
+    print('-' * 45)
+
+
+def cor(txt, num, continua=False):
+    print(f'\033[{num}m {txt} \033[m') if continua is False else print(f'\033[{num}m {txt} \033[m', end='')
+
+
+opcao = 0
+while True:
+    exibir('MENU PRINCIPAL')
+    cor('1 -', 33, True)
+    cor('Ver pessoas cadastradas', 34, False)
+    cor('2 -', 33, True)
+    cor('Cadastrar nova Pessoa', 34, False)
+    cor('3 -', 33, True)
+    cor('Sair do Sistema', 34, False)
+    print('-' * 45)
+    try:
+        opcao = int(input('\033[33m Sua opção: \033[m'))
+        if opcao == 3:
+            exibir('Saindo do sistema... Até logo !')
+            break
+        elif opcao == 2:
+            exibir('NOVO CADASTRO')
+            nome = str(input('Nome: '))
+            idade = int(input('Idade: '))
+            print(f'Novo registro de {nome} adicionado')
+            with open('pessoas.txt', 'a') as arquivo:
+                texto = f'{nome} {idade:>25} anos'
+                arquivo.write(texto + '\n')
+            sleep(1)
+        elif opcao == 1:
+            exibir('PESSOAS CADASTRADAS')
+            with open('pessoas.txt', 'r') as arquivo:
+                for linha in arquivo:
+                    linha = linha.rstrip()
+                    print(linha)
+            sleep(1)
+        else:
+            cor('ERRO! Digite uma opção válida!', 31)
+
+    except:
+        cor('ERRO! Digite uma opção válida!', 31)
